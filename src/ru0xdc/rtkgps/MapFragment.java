5e510f1ea6b42efe28ce2702daa71ce414576aa4
Alexey Illarionov <littlesavage@rambler.ru>
2013-06-14 07:43:39 +0400

    Bing

--- src/ru0xdc/rtkgps/MapFragment.java
+++ src/ru0xdc/rtkgps/MapFragment.java
@@ -8,6 +8,7 @@ import java.util.TimerTask;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.bing.BingMapTileSource;
 import org.osmdroid.views.MapView;
 
 import ru0xdc.rtkgps.view.StreamIndicatorsView;
@@ -18,6 +19,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import butterknife.InjectView;
@@ -34,9 +38,15 @@ public class MapFragment extends Fragment {
     private static final String PREFS_SCROLL_Y = "scroll_y";
     private static final String PREFS_ZOOM_LEVEL = "zoom_level";
 
+    private static final String MAP_MODE_BING="BingMap";
+    private static final String MAP_MODE_BING_AERIAL="Bing aerial";
+    private static final String MAP_MODE_BING_ROAD="Bing road";
+
     private Timer mStreamStatusUpdateTimer;
     private RtkServerStreamStatus mStreamStatus;
     private ResourceProxy mResourceProxy;
+
+    private BingMapTileSource mBingRoadTileSource, mBingAerialTileSource;
 
     @InjectView(R.id.streamIndicatorsView) StreamIndicatorsView mStreamIndicatorsView;
     @InjectView(R.id.map_container) ViewGroup mMapViewContainer;
@@ -52,6 +62,18 @@ public class MapFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         mResourceProxy = new ResourceProxyImpl(activity.getApplicationContext());
+        BingMapTileSource.retrieveBingKey(activity);
+        mBingRoadTileSource = new BingMapTileSource(null);
+        mBingRoadTileSource.setStyle(BingMapTileSource.IMAGERYSET_ROAD);
+        mBingAerialTileSource = new BingMapTileSource(null);
+        mBingAerialTileSource.setStyle(BingMapTileSource.IMAGERYSET_AERIAL);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -72,7 +94,6 @@ public class MapFragment extends Fragment {
 
         return v;
     }
-
 
     @Override
     public void onStart() {
@@ -104,6 +125,32 @@ public class MapFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_map, menu);
+    }
+
+
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        final int checked;
+
+        if (mMapView == null) return;
+
+        final String providerName = getTileSourceName();
+        if (MAP_MODE_BING_AERIAL.equals(providerName)) {
+            checked = R.id.menu_map_mode_bing_aerial;
+        }else if (MAP_MODE_BING_ROAD.equals(providerName)) {
+            checked = R.id.menu_map_mode_bing_road;
+        }else {
+            checked = R.id.menu_map_mode_osm;
+        }
+
+        menu.findItem(checked).setChecked(true);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         loadMapPreferences();
@@ -117,13 +164,35 @@ public class MapFragment extends Fragment {
 
     }
 
-
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         mMapView = null;
         Views.reset(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        final String tileSource;
+
+        switch (item.getItemId()) {
+        case R.id.menu_map_mode_osm:
+            tileSource = TileSourceFactory.MAPNIK.name();
+            break;
+        case R.id.menu_map_mode_bing_aerial:
+            tileSource = MAP_MODE_BING_AERIAL;
+            break;
+        case R.id.menu_map_mode_bing_road:
+            tileSource = MAP_MODE_BING_ROAD;
+            break;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+
+        setTileSource(tileSource);
+
+        return true;
     }
 
     void updateStatus() {
@@ -155,7 +224,7 @@ public class MapFragment extends Fragment {
         getActivity()
             .getSharedPreferences(SHARED_PREFS_NAME,Context.MODE_PRIVATE)
             .edit()
-            .putString(PREFS_TITLE_SOURCE, mMapView.getTileProvider().getTileSource().name())
+            .putString(PREFS_TITLE_SOURCE, getTileSourceName())
             .putInt(PREFS_SCROLL_X, mMapView.getScrollX())
             .putInt(PREFS_SCROLL_Y, mMapView.getScrollY())
             .putInt(PREFS_ZOOM_LEVEL, mMapView.getZoomLevel())
@@ -167,11 +236,7 @@ public class MapFragment extends Fragment {
         SharedPreferences prefs = getActivity().getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
 
         final String tileSourceName = prefs.getString(PREFS_TITLE_SOURCE, TileSourceFactory.DEFAULT_TILE_SOURCE.name());
-        try {
-            final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
-            mMapView.setTileSource(tileSource);
-        } catch (final IllegalArgumentException ignore) {
-        }
+        setTileSource(tileSourceName);
 
         mMapView.getController().setZoom(prefs.getInt(PREFS_ZOOM_LEVEL, 1));
 
@@ -179,6 +244,40 @@ public class MapFragment extends Fragment {
                 prefs.getInt(PREFS_SCROLL_X, 0),
                 prefs.getInt(PREFS_SCROLL_Y, 0)
                 );
+    }
+
+    private void setTileSource(String name) {
+        ITileSource tileSource;
+
+        if (MAP_MODE_BING_AERIAL.equals(name)) {
+            tileSource = mBingAerialTileSource;
+        }else if (MAP_MODE_BING_ROAD.equals(name)) {
+            tileSource = mBingRoadTileSource;
+        }else {
+            try {
+                tileSource = TileSourceFactory.getTileSource(name);
+            }catch(IllegalArgumentException iae) {
+                tileSource = TileSourceFactory.MAPNIK;
+            }
+        }
+
+        if (!tileSource.equals(mMapView.getTileProvider().getTileSource())) {
+            mMapView.setTileSource(tileSource);
+        }
+    }
+
+    private String getTileSourceName() {
+        final ITileSource provider = mMapView.getTileProvider().getTileSource();
+
+        if (MAP_MODE_BING.equals(provider.name())) {
+            if (BingMapTileSource.IMAGERYSET_ROAD.equals(((BingMapTileSource)provider).getStyle())) {
+                return MAP_MODE_BING_ROAD;
+            }else {
+                return MAP_MODE_BING_AERIAL;
+            }
+        }else {
+            return provider.name();
+        }
     }
 
 }
